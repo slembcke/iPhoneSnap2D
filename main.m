d53582cf5803bdc6b63bff42ac076a66ac107f5d
slembcke <slembcke@gmail.com>
2012-11-20 11:57:25 -0600

    initial import

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iPhoneSnap
+//
+//  Created by Scott Lembcke on 2/10/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	sranddev();
+
+	@autoreleasepool {
+		return UIApplicationMain(argc, argv, nil, nil);
+	}
+}
